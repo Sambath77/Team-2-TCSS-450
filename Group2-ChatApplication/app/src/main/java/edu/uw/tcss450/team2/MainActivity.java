b4c25571c4a583049b0b8422abd0eed07da234c9
Sambath <sambathpech77@gmail.com>
2020-11-02 18:25:24 -0800

    tool bar added

--- Group2-ChatApplication/app/src/main/java/edu/uw/tcss450/team2/MainActivity.java
+++ Group2-ChatApplication/app/src/main/java/edu/uw/tcss450/team2/MainActivity.java
@@ -1,29 +1,69 @@
 package edu.uw.tcss450.team2;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toolbar;
 
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import edu.uw.tcss450.team2.fragments.FriendFragment;
+import edu.uw.tcss450.team2.fragments.GalleryFragment;
+import edu.uw.tcss450.team2.fragments.HistoryFragment;
+import edu.uw.tcss450.team2.fragments.LogoutFragment;
+import edu.uw.tcss450.team2.fragments.MoreFragment;
+import edu.uw.tcss450.team2.fragments.ProfileFragment;
+import edu.uw.tcss450.team2.fragments.SettingFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private AppBarConfiguration mAppBarConfiguration;
+    Toolbar toolbar, topBar;
+    DrawerLayout mDrawLayout;
+    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Find the toolbar Id
+        //drawer toolbar
+        toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        //Appbar id
+        topBar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(topBar);
+
+        //main_layout is the id activity_main
+        mDrawLayout = findViewById(R.id.main_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawLayout,
+                toolbar, R.string.open, R.string.close);
+
+        mDrawLayout.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.drawer_nav);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //NavigationView navigationView1 = findViewById(R.id.main_toolbar);
+
+
         //MainActivityArgs args = MainActivityArgs.fromBundle(getIntent().getExtras());
         //String email = args.getEmail();
         //String jwt = args.getJwt();
@@ -45,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
 
     @Override
@@ -56,20 +97,56 @@ public class MainActivity extends AppCompatActivity {
 
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.toobar_menu, menu);
+//        getMenuInflater().inflate(R.menu.top_menu, menu);
 //        return true;
 //    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//
-//        if (id == R.id.search_bar || id == R.id.notification_bar) {
-//            //TODO open a settting fragment
-//            Log.d("SETTING", "Clicked");
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.profile:
+                showDrawerFragment(new ProfileFragment());
+                break;
+            case R.id.friend:
+                showDrawerFragment(new FriendFragment());
+                break;
+            case R.id.gallery:
+                showDrawerFragment(new GalleryFragment());
+                break;
+            case R.id.history:
+                showDrawerFragment(new HistoryFragment());
+                break;
+            case R.id.setting:
+                showDrawerFragment(new SettingFragment());
+                break;
+            case R.id.log_out:
+                showDrawerFragment(new LogoutFragment());
+                break;
+            default:
+                showDrawerFragment(new MoreFragment());
+                break;
+        }
+        mDrawLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void showDrawerFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawLayout.closeDrawer(GravityCompat.START);
+
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 
 }
---
+++ Group2-ChatApplication/app/src/main/java/edu/uw/tcss450/team2/fragments/SettingFragment.java
@@ -0,0 +1,26 @@
+package edu.uw.tcss450.team2.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.team2.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SettingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SettingFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting, container, false);
+    }
+}
