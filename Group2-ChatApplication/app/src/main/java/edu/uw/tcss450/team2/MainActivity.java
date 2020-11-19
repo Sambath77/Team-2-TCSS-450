80980c2d48fcd803507507173d5b273899f3ec14
supersam710 <spillers.sam@gmail.com>
2020-11-19 05:58:18 -0800

    Revert last commit

--- Group2-ChatApplication/app/src/main/java/edu/uw/tcss450/team2/MainActivity.java
+++ Group2-ChatApplication/app/src/main/java/edu/uw/tcss450/team2/MainActivity.java
@@ -14,15 +14,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+<<<<<<< HEAD
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+=======
+>>>>>>> parent of bb0b8ef... Merge pull request #61 from Sambath77/Hyeong
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+<<<<<<< HEAD
 import androidx.appcompat.app.ActionBar;
 
 
+=======
+>>>>>>> parent of bb0b8ef... Merge pull request #61 from Sambath77/Hyeong
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDestination;
@@ -35,6 +41,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+<<<<<<< HEAD
 import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -48,19 +55,26 @@ import android.content.Intent;
 
 import android.os.Bundle;
 import android.util.Log;
+=======
+import android.os.Bundle;
+>>>>>>> parent of bb0b8ef... Merge pull request #61 from Sambath77/Hyeong
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.appbar.MaterialToolbar;
+<<<<<<< HEAD
 
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
+=======
+>>>>>>> parent of bb0b8ef... Merge pull request #61 from Sambath77/Hyeong
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.navigation.NavigationView;
+<<<<<<< HEAD
 
 //import edu.uw.tcss450.team2.fragments.FriendFragment;
 //import edu.uw.tcss450.team2.fragments.GalleryFragment;
@@ -95,8 +109,30 @@ import edu.uw.tcss450.team2.model.NewMessageCountViewModel;
 import edu.uw.tcss450.team2.model.UserInfoViewModel;
 import edu.uw.tcss450.team2.services.PushReceiver;
 import edu.uw.tcss450.team2.signin.SignInFragment;
+=======
+import com.google.android.material.radiobutton.MaterialRadioButton;
+import com.google.android.material.snackbar.Snackbar;
 
+import edu.uw.tcss450.team2.chat.ChatMessage;
+import edu.uw.tcss450.team2.chat.ChatViewModel;
+import edu.uw.tcss450.team2.databinding.ActivityMainBinding;
+import edu.uw.tcss450.team2.fragments.FriendFragment;
+import edu.uw.tcss450.team2.fragments.GalleryFragment;
+import edu.uw.tcss450.team2.fragments.HistoryFragment;
+import edu.uw.tcss450.team2.fragments.LogoutFragment;
+import edu.uw.tcss450.team2.fragments.MoreFragment;
+import edu.uw.tcss450.team2.fragments.ProfileFragment;
+import edu.uw.tcss450.team2.fragments.SettingFragment;
+import edu.uw.tcss450.team2.model.NewMessageCountViewModel;
+import edu.uw.tcss450.team2.model.UserInfoViewModel;
+import edu.uw.tcss450.team2.services.PushReceiver;
+import edu.uw.tcss450.team2.signin.SignInFragment;
 
+>>>>>>> parent of bb0b8ef... Merge pull request #61 from Sambath77/Hyeong
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+<<<<<<< HEAD
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private MainPushMessageReceiver mPushMessageReceiver;
@@ -106,10 +142,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private AppBarConfiguration mAppBarConfiguration;
 	
+=======
+    private MainPushMessageReceiver mPushMessageReceiver;
+    private NewMessageCountViewModel mNewMessageModel;
+
+    private ActivityMainBinding binding;
+
+    private AppBarConfiguration mAppBarConfiguration;
+>>>>>>> parent of bb0b8ef... Merge pull request #61 from Sambath77/Hyeong
     Toolbar toolbar;
     DrawerLayout mDrawLayout;
     ActionBarDrawerToggle mDrawerToggle;
     private SignInFragment signInFragment;
+<<<<<<< HEAD
     //    private GpsTracker gpsTracker;
 //    private TextView tvLatitude,tvLongitude;
     double longitude;
@@ -123,6 +168,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     String email;
     String jwt;
 
+=======
+>>>>>>> parent of bb0b8ef... Merge pull request #61 from Sambath77/Hyeong
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -143,6 +190,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mDrawLayout = findViewById(R.id.main_layout);
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawLayout,
                 toolbar, R.string.open, R.string.close);
+<<<<<<< HEAD
 
         mDrawLayout.addDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
@@ -150,6 +198,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = findViewById(R.id.drawer_nav);
         navigationView.setNavigationItemSelectedListener(this);
 
+=======
+
+        mDrawLayout.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.drawer_nav);
+        navigationView.setNavigationItemSelectedListener(this);
+
+>>>>>>> parent of bb0b8ef... Merge pull request #61 from Sambath77/Hyeong
         //NavigationView navigationView1 = findViewById(R.id.main_toolbar);
 
 
@@ -206,6 +263,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
              */
         });
+<<<<<<< HEAD
 //        //TODO handlong getting current lat/long
 //        tvLatitude = (TextView)findViewById(R.id.latitude);
 //        tvLongitude = (TextView)findViewById(R.id.longitude);
@@ -228,6 +286,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 //        latitude = location.getLatitude();
 //        longitude = location.getLongitude();
 //        System.out.println("lat/long: " + latitude + longitude);
+=======
+
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController1 = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController1, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+>>>>>>> parent of bb0b8ef... Merge pull request #61 from Sambath77/Hyeong
 
 //        findViewById(R.id.navigation_home).setOnClickListener(new View.OnClickListener() {
 //
@@ -259,6 +329,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 //        }
 
 
+<<<<<<< HEAD
 //    }
 
     @Override
@@ -271,6 +342,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
     @Override
+=======
+    @Override
+>>>>>>> parent of bb0b8ef... Merge pull request #61 from Sambath77/Hyeong
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
         int id = item.getItemId();
@@ -317,6 +391,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         mDrawLayout.closeDrawer(GravityCompat.START);
         return true;
+<<<<<<< HEAD
     }
 
     /*
@@ -330,6 +405,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         fragmentTransaction.commit();
     }
 
+=======
+    }
+
+    /*
+     * Helper class to navigate a item in menu to a new  fragment
+     * @params: Fragement
+     *
+     */
+    private void showDrawerFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+>>>>>>> parent of bb0b8ef... Merge pull request #61 from Sambath77/Hyeong
 
     @Override
     public void onBackPressed() {
@@ -339,6 +429,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else {
             super.onBackPressed();
         }
+<<<<<<< HEAD
+=======
+    }
+
+    /**
+     * A BroadcastReceiver that listens for messages sent from PushReceiver
+     */
+    private class MainPushMessageReceiver extends BroadcastReceiver {
+        private ChatViewModel mModel =
+                new ViewModelProvider(MainActivity.this)
+                        .get(ChatViewModel.class);
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            NavController nc =
+                    Navigation.findNavController(
+                            MainActivity.this, R.id.nav_host_fragment);
+            NavDestination nd = nc.getCurrentDestination();
+            if (intent.hasExtra("chatMessage")) {
+                ChatMessage cm = (ChatMessage) intent.getSerializableExtra("chatMessage");
+                //If the user is not on the chat screen, update the
+                // NewMessageCountView Model
+                if (nd.getId() != R.id.navigation_chat) {
+                    mNewMessageModel.increment();
+                }
+                //Inform the view model holding chatroom messages of the new
+                //message.
+                mModel.addMessage(intent.getIntExtra("chatid", -1), cm);
+            }
+        }
+>>>>>>> parent of bb0b8ef... Merge pull request #61 from Sambath77/Hyeong
     }
 
     /**
@@ -378,6 +498,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         registerReceiver(mPushMessageReceiver, iFilter);
     }
     @Override
+<<<<<<< HEAD
     public void onPause() {
         super.onPause();
         if (mPushMessageReceiver != null){
@@ -450,3 +571,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 //            super.onBackPressed();
 //        }
 //    }
+=======
+    public void onResume() {
+        super.onResume();
+        if (mPushMessageReceiver == null) {
+            mPushMessageReceiver = new MainPushMessageReceiver();
+        }
+        IntentFilter iFilter = new IntentFilter(PushReceiver.RECEIVED_NEW_MESSAGE);
+        registerReceiver(mPushMessageReceiver, iFilter);
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mPushMessageReceiver != null){
+            unregisterReceiver(mPushMessageReceiver);
+        }
+    }
+
+
+
+}
+>>>>>>> parent of bb0b8ef... Merge pull request #61 from Sambath77/Hyeong
