a703e703a82f11cff765c4dd7bc4a76a0238392d
supersam710 <spillers.sam@gmail.com>
2020-11-19 05:51:21 -0800

    Fixing up the merge

--- Group2-ChatApplication/app/src/main/java/edu/uw/tcss450/team2/home/HomeFragment.java
+++ Group2-ChatApplication/app/src/main/java/edu/uw/tcss450/team2/home/HomeFragment.java
@@ -108,7 +108,7 @@ public class HomeFragment extends Fragment /*implements OnMapReadyCallback*/ {
 
         binding.layoutHomeUnreadChat.setOnClickListener(layout ->
                 Navigation.findNavController(getView()).navigate(
-                        HomeFragmentDirections.actionNavigationHomeToChatFragment()));
+                        HomeFragmentDirections.actionNavigationHomeToNavigationChat()));
 
         binding.layoutHomeWeather.setOnClickListener(layout ->
                 Navigation.findNavController(getView()).navigate(
